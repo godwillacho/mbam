c5553be4f82a5816a57c2a18d4dfb5e715611661
Acho <77398859+godwillacho@users.noreply.github.com>
2026-06-11 19:10:20 +0400

    Load cleaner dashboard translation bundle

--- mbam-web/src/main.tsx
+++ mbam-web/src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./i18n";
 import "./i18n/roleDashboardResources";
 import "./i18n/productRevenueResources";
+import "./i18n/cleanDashboardResources";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
