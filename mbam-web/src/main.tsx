6e1250210bc35949d0856873b36afc9946710f76
Acho <77398859+godwillacho@users.noreply.github.com>
2026-06-11 18:20:40 +0400

    Load product revenue translation bundle

--- mbam-web/src/main.tsx
+++ mbam-web/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./i18n";
 import "./i18n/roleDashboardResources";
+import "./i18n/productRevenueResources";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
