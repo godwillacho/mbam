f9d7e628a7d0cef8ca456314347d538ddd39edad
Acho <77398859+godwillacho@users.noreply.github.com>
2026-06-11 04:38:32 +0400

    Add protected route wrapper

---
+++ mbam-web/src/components/app/ProtectedRoute.tsx
@@ -0,0 +1,17 @@
+import { Navigate } from "react-router-dom";
+import { canAccessRoute, getCurrentMember, type AppRouteKey } from "../../security/accessControl";
+
+interface ProtectedRouteProps {
+  routeKey: AppRouteKey;
+  children: JSX.Element;
+}
+
+export default function ProtectedRoute({ routeKey, children }: ProtectedRouteProps) {
+  const member = getCurrentMember();
+
+  if (!canAccessRoute(member, routeKey)) {
+    return <Navigate to="/dashboard" replace />;
+  }
+
+  return children;
+}
